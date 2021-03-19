2b674d28f636d107f783b7fe74dd7a924a43fc52
Cyzorz <cyzorzyes@gmail.com>
2021-03-18 22:28:17 -0700

    Minor updates

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/home')
 @app.route('/')
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+@app.route('/home/contact')
+def bio():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
