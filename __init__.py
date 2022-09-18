241dc347520bb30ce310fcbce47afb937bdefe7d
Cyzorz <cyzorzyes@gmail.com>
2022-09-18 23:56:36 +0200

    yes

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/home')
@@ -9,6 +9,10 @@ def home():
 @app.route('/contact')
 def bio():
     return render_template('contact.html')
+
+@app.route('/map')
+def renderMap():
+    return redirect("http://104.168.218.142:8123/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
